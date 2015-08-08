e16785204678ecf3d12bd3276e592349ac738cf1
koox00 <cooxlee@gmail.com>
2015-08-08 14:26:45 +0300

    first problem

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,16 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+// $("#main").append("Sofia Roumeliotou<br>");
+//
+// var awesomeThoughts = "I am sofia and i love koulis and i am awesome";
+// console.log(awesomeThoughts);
+// var fun = awesomeThoughts.replace("awesome", "fun");
+// $("#main").append(fun+"<br>");
+var name = "Sofia Roumeliotou";
+var role = "web Developer";
+var formattedName = HTMLheaderName.replace("%data%",name);
+var formattedRole = HTMLheaderRole.replace("%data%",role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
